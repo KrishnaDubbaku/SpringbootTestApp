d106202048e40f89581be26650086a9126b27da5
lenovo <lenovo@DESKTOP-V1R6DAL>
2021-06-08 13:07:19 +0530

    WH-8520-First Time Commit

---
+++ src/main/java/boot/git/SpringbootTestAppApplication.java
@@ -0,0 +1,13 @@
+package boot.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/boot/git/messasge/MessageRunner.java
@@ -0,0 +1,16 @@
+package boot.git.messasge;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner
+{
+
+	@Override
+	public void run(String... args) throws Exception 
+	{
+ 		System.out.println("WELCOME TO SPRING BOOOT USING GITHUB");
+	}
+
+}
