9220a739a254e7f0bd652179ee4652b44902ce09
lenovo <lenovo@DESKTOP-V1R6DAL>
2021-06-08 21:32:53 +0530

    WH-6894

--- src/main/java/boot/git/messasge/MessageRunner.java
+++ src/main/java/boot/git/messasge/MessageRunner.java
@@ -1,18 +1,29 @@
 package boot.git.messasge;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner
 {
-
+private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception 
 	{
 		int a=10;
- 		System.out.println("HELLO GIT HUB ");
-		System.out.println("WELCOME TO SPRING BOOOT USING GITHUB"+a);
+		try
+		{
+			log.info("STARTED");
+			System.out.println("HELLO GIT HUB ");
+			System.out.println("WELCOME TO SPRING BOOOT USING GITHUB"+a);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		log.info("END");
 	}
 
 }
